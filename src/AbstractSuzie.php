c444e20b031198c8e283c23673f7599e9326d6c5
atlantisgroupbv <newcityofatlantis@gmail.com>
2024-02-09 17:51:16 +0100

    cleanup

--- src/AbstractSuzie.php
+++ src/AbstractSuzie.php
@@ -3,13 +3,31 @@
 namespace KooijmanInc\Suzie;
 
 
+use KooijmanInc\Suzie\Model\DataAccess\AbstractDataAccess;
 use KooijmanInc\Suzie\Model\DataAccess\DataAccessInterface;
+use Symfony\Contracts\Service\Attribute\Required;
 
 abstract class AbstractSuzie implements SuzieInterface
 {
+    /**
+     * @var DataAccessInterface
+     */
+    protected DataAccessInterface $dataAccess;
 
-    public function __construct(DataAccessInterface $dataAccess)
+    /**
+     * @param AbstractDataAccess $dataAccess
+     * @return AbstractSuzie
+     */
+    #[Required]
+    public function init(AbstractDataAccess $dataAccess)
     {
-        dump($dataAccess);
+        $this->dataAccess = $dataAccess;
+
+        return $this;
+    }
+
+    public function __construct()
+    {
+        dump();
     }
 }
