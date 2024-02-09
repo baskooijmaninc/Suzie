6edee596348248f1bf18d12bf97a3a225c80a16c
atlantisgroupbv <newcityofatlantis@gmail.com>
2024-02-09 18:24:42 +0100

    cleanup

--- src/Model/DataAccess/AbstractDataAccess.php
+++ src/Model/DataAccess/AbstractDataAccess.php
@@ -3,6 +3,7 @@
 namespace KooijmanInc\Suzie\Model\DataAccess;
 
 use KooijmanInc\Suzie\Model\Connection\ConnectionFactoryInterface;
+use Psr\Log\LoggerInterface;
 
 abstract class AbstractDataAccess implements DataAccessInterface
 {
@@ -11,5 +12,19 @@ abstract class AbstractDataAccess implements DataAccessInterface
     public function __construct(ConnectionFactoryInterface $connectionFactory, bool $debug = false)
     {
         $this->debug = $debug;
+    }
+
+    public function setLogger(LoggerInterface $logger = null): DataAccessInterface
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+
+    public function setDebug(bool $debug): DataAccessInterface
+    {
+        $this->debug = $debug;
+
+        return $this;
     }
 }
--- src/Model/DataAccess/DataAccessInterface.php
+++ src/Model/DataAccess/DataAccessInterface.php
@@ -2,7 +2,19 @@
 
 namespace KooijmanInc\Suzie\Model\DataAccess;
 
+use Psr\Log\LoggerInterface;
+
 interface DataAccessInterface
 {
+    /**
+     * @param LoggerInterface|null $logger
+     * @return DataAccessInterface
+     */
+    public function setLogger(LoggerInterface $logger = null): DataAccessInterface;
 
+    /**
+     * @param bool $debug
+     * @return DataAccessInterface
+     */
+    public function setDebug(bool $debug): DataAccessInterface;
 }
