4d93e928626bf9814a963a86f856633b863fb73c
atlantisgroupbv <newcityofatlantis@gmail.com>
2024-02-09 19:14:03 +0100

    cleanup

--- src/AbstractSuzie.php
+++ src/AbstractSuzie.php
@@ -3,24 +3,44 @@
 namespace KooijmanInc\Suzie;
 
 use KooijmanInc\Suzie\Model\DataAccess\DataAccessInterface;
+use Psr\Log\LoggerInterface;
 
 abstract class AbstractSuzie implements SuzieInterface
 {
     /**
      * @var DataAccessInterface
      */
-    protected $dataAccess;
+    protected DataAccessInterface $dataAccess;
 
-    protected $debug;
+    /**
+     * @var bool
+     */
+    protected bool $debug;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected LoggerInterface $logger;
+
+    /**
+     * @var string
+     */
+    protected string $name;
 
     /**
      * @param DataAccessInterface $dataAccess
+     * @param LoggerInterface|null $logger
      * @param bool $debug
      */
-    public function __construct(DataAccessInterface $dataAccess, bool $debug = false)
+    public function __construct(DataAccessInterface $dataAccess, LoggerInterface $logger = null, bool $debug = false)
     {
         $this->dataAccess = $dataAccess;
+        $this->logger = $logger;
         $this->debug = $debug;
-        dump($this);
+
+        $this->name = get_called_class();
+
+        $this->dataAccess->setLogger($this->logger);
+        $this->dataAccess->setDebug($this->debug);
     }
 }
