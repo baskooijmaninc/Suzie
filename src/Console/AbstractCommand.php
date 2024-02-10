b9356f6406e2f4786d1a0d10e71fb8f8c7b24e44
kooijmanincorporate <kooijmanincorporate@gmail.com>
2024-02-10 20:26:03 +0100

    creating console command

---
+++ src/Console/AbstractCommand.php
@@ -0,0 +1,1 @@
+<?phpnamespace KooijmanInc\Suzie\Console\Command;use Symfony\Component\Console\Command\Command;abstract class AbstractCommand extends Command{    protected function configure()    {    }}
