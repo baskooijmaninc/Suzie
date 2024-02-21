7e81c8180eb77ecd2d6c56848ab6eda1f85e4d8a
atlantisgroupbv <newcityofatlantis@gmail.com>
2024-02-21 14:27:59 +0100

    implement validation

--- src/FormValidation/AbstractFormValidation.php
+++ src/FormValidation/AbstractFormValidation.php
@@ -10,5 +10,13 @@ use Symfony\Component\HttpFoundation\RequestStack;
  */
 abstract class AbstractFormValidation implements FormValidationInterface
 {
+    public function &__get(string $name)
+    {
+        // TODO: Implement __get() method.
+    }
 
+    public function __set(string $name, $value)
+    {
+        // TODO: Implement __set() method.
+    }
 }
