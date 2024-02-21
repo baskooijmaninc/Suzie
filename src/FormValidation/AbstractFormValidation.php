b08402c4d3abcc2e490aa5587b373b98d7fda7fb
atlantisgroupbv <newcityofatlantis@gmail.com>
2024-02-21 14:26:11 +0100

    implement validation

---
+++ src/FormValidation/AbstractFormValidation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KooijmanInc\Suzie\FormValidation;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Class AbstractFormValidation
+ * @package KooijmanInc\Suzie\FormValidation
+ */
+abstract class AbstractFormValidation implements FormValidationInterface
+{
+
+}
---
+++ src/FormValidation/FormValidationInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KooijmanInc\Suzie\FormValidation;
+
+use KooijmanInc\Suzie\FormBuilder\FormCollector\FormCollectorInterface;
+
+/**
+ * Interface FormValidationInterface
+ */
+interface FormValidationInterface extends FormCollectorInterface
+{
+
+}
